f567d51271734d93fe7a2c395ce7444cb4703a3c
Alex Malakov <32112772+AlexMalakov@users.noreply.github.com>
2022-12-27 19:00:17 -0500

    AAHHHHHHHHHHHHHHHHHHH BITRICH AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH BITRICHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH

--- src/main/malakov.tradingbot/Main.java
+++ src/main/malakov.tradingbot/Main.java
@@ -1,9 +1,10 @@
+
+//import org.knowm.xchange.coinbasepro.CoinbaseProExchange;
 import info.bitrich.xchangestream.coinbasepro.CoinbaseProStreamingExchange;
 import info.bitrich.xchangestream.core.ProductSubscription;
 import info.bitrich.xchangestream.core.StreamingExchange;
 import info.bitrich.xchangestream.core.StreamingExchangeFactory;
 
-import org.checkerframework.checker.units.qual.A;
 import org.knowm.xchange.currency.CurrencyPair;
 import org.knowm.xchange.dto.marketdata.Trade;
 
@@ -13,20 +14,15 @@ import java.util.ArrayList;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import io.reactivex.disposables.Disposable;
+import orderbook.MyOrderBook;
 
 public class Main {
 
   final static int INFORMATION_CAPTURE_DURATION = 20000;
 
   public static void main(String[] args) throws IOException, InterruptedException {
-    System.out.println("hello world");
 
-//    Exchange coinbase = ExchangeFactory.INSTANCE.createExchange(CoinbaseProExchange.class);
-//    MarketDataService marketDataService = coinbase.getMarketDataService();
-//    Ticker ticker = marketDataService.getTicker(CurrencyPair.BTC_USD);
-//    System.out.println(ticker.toString());
 
-    // Use StreamingExchangeFactory instead of ExchangeFactory
     StreamingExchange exchange = StreamingExchangeFactory.INSTANCE.createExchange(CoinbaseProStreamingExchange.class);
 
 // Connect to the Exchange WebSocket API. Here we use a blocking wait.
@@ -37,24 +33,25 @@ public class Main {
             .build();
     exchange.connect(subscription).blockingAwait();
 
-    AtomicInteger tradeCounter = new AtomicInteger();
-    ArrayList<Trade> tradesList = new ArrayList<>();
+
 // Subscribe to live trades update.
     Disposable subscription1 = exchange.getStreamingMarketDataService()
             .getTrades(CurrencyPair.BTC_USD)
             .subscribe(
                     trade -> {
-                      System.out.println("Trade number" + tradeCounter.getAndIncrement() + ": {" + trade + "}");
-                      tradesList.add(trade);
+                      System.out.println("trade: " + trade);
                     },
                     throwable -> System.err.println("Trade didn't work: " + throwable));
 
 // Subscribe order book data with the reference to the subscription.
+    MyOrderBook book = new MyOrderBook();
+
     Disposable subscription2 = exchange.getStreamingMarketDataService()
             .getOrderBook(CurrencyPair.BTC_USD)
             .subscribe(orderBook -> {
-              System.out.println("OrderBook: {" + orderBook + "}");
-
+//              System.out.println("OrderBook: {" + orderBook + "}");
+                book.update(orderBook);
+                System.out.println(book.toString());
             });
 
 // Wait for a while to see some results arrive
@@ -66,27 +63,5 @@ public class Main {
 
 // Disconnect from exchange (blocking again)
     exchange.disconnect().blockingAwait();
-
-    System.out.println("Amount of trades during " + INFORMATION_CAPTURE_DURATION + "ms: " + tradeCounter);
-
-    BigDecimal[] chunkSum = new BigDecimal[5];
-    int[] cantbeBotheredToCalcThis = new int[5];
-
-    for(int i = 0; i < 5; i++) {
-      chunkSum[i] = new BigDecimal(0);
-      cantbeBotheredToCalcThis[i] = 0;
-    }
-
-    for(int i = 0; i < tradesList.size(); i++) {
-      cantbeBotheredToCalcThis[(i*5)/tradesList.size()]++;
-      chunkSum[(i*5)/tradesList.size()].add(tradesList.get(i).getPrice());
-      System.out.println("price of trade " + i + ": " + tradesList.get(i).getPrice());
-    }
-    for(int i = 0; i < 5; i++) {
-      chunkSum[i].divide(new BigDecimal(cantbeBotheredToCalcThis[i]));
-    }
-    for(BigDecimal b: chunkSum) {
-      System.out.println("AVERAGE OF A FEW TRADES: " + b);
-    }
   }
 }
