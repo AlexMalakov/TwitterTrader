d6c08857089c7ea561602458f96a59ed1592c089
Alex Malakov <32112772+AlexMalakov@users.noreply.github.com>
2022-12-31 13:02:24 -0500

    Linking Twitter indicator to bot

--- src/main/java/malakov/tradingbot/Bot.java
+++ src/main/java/malakov/tradingbot/Bot.java
@@ -5,181 +5,91 @@ import info.bitrich.xchangestream.core.ProductSubscription;
 import info.bitrich.xchangestream.core.StreamingExchangeFactory;
 
 import org.apache.commons.lang3.StringUtils;
-import org.knowm.xchange.Exchange;
 import org.knowm.xchange.ExchangeSpecification;
 import org.knowm.xchange.currency.CurrencyPair;
 import org.knowm.xchange.dto.Order;
 import org.knowm.xchange.dto.marketdata.OrderBook;
+import org.knowm.xchange.dto.marketdata.Trade;
 import org.knowm.xchange.dto.trade.LimitOrder;
 import org.knowm.xchange.dto.trade.UserTrade;
-import org.knowm.xchange.instrument.Instrument;
 
 import java.io.Closeable;
 import java.io.IOException;
 import java.math.BigDecimal;
-import java.math.MathContext;
 import java.math.RoundingMode;
 import java.util.Date;
 
 import io.reactivex.disposables.Disposable;
-import malakov.tradingbot.orderbook.OrderBookPriceCalculator;
+import malakov.tradingbot.orderbook.Exchange;
+import malakov.tradingbot.twitter.TwitterExplorer;
 
 public class Bot implements Closeable {
 
-  private final ExchangeSpecification spec;
-  private final ProductSubscription productSubscription;
-  private CoinbaseProStreamingExchange exchange;
-  private Disposable subscription;
-  private Disposable subsciption2;
-  private Disposable subsciption3;
   private enum State {
-          INIT, BUY, WAITFORFILL, SELL, WAITFORSELLEND, LEAVE
+          INIT, FIND_INDICATOR,BUY, WAIT_FOR_FULFILLED_BID, SELL, WAIT_FOR_FULFILLED_ASK, LEAVE
   }
 
-  State state;
+  private State state;
+  private final Exchange exchange;
+  private final BigDecimal tradePrice;
+  private final TwitterExplorer indicatorFinder;
 
 
+  public Bot(double tradePrice) {
 
-  public Bot() {
-
-
-    productSubscription =
-            ProductSubscription.create()
-                    .addTicker(CurrencyPair.BTC_USD)
-                    .addOrders(CurrencyPair.BTC_USD)
-                    .addOrderbook(CurrencyPair.BTC_USD)
-                    .addUserTrades(CurrencyPair.BTC_USD)
-                    .build();
-
-    spec =
-            StreamingExchangeFactory.INSTANCE
-                    .createExchange(CoinbaseProStreamingExchange.class)
-                    .getDefaultExchangeSpecification();
-
-    System.out.println("proceeding");
+    this.tradePrice = new BigDecimal(tradePrice);
+    exchange = new Exchange(CurrencyPair.BTC_USD);
+    this.indicatorFinder = new TwitterExplorer("a", "b");
+    this.state = State.INIT;
   }
 
   public void init() {
-
-    state = State.INIT;
-
-    String apiKey = System.getenv("api-key");
-    String apiSecret = System.getenv("api-secret");
-    String apiPassphrase = System.getenv("passphrase");
-
-    spec.setApiKey(apiKey);
-    spec.setSecretKey(apiSecret);
-    spec.setExchangeSpecificParametersItem("passphrase", apiPassphrase);
-    spec.setExchangeSpecificParametersItem(Exchange.USE_SANDBOX, true);
-
-    exchange =
-            (CoinbaseProStreamingExchange) StreamingExchangeFactory.INSTANCE.createExchange(spec);
-
-    System.out.println("proceeding again");
-    exchange.connect(productSubscription).blockingAwait();
-
-    System.out.println("past the block await");
-    state = State.BUY;
-
-    subscription = exchange
-            .getStreamingMarketDataService()
-            .getOrderBook(CurrencyPair.BTC_USD)
-            .subscribe((this::update));
-
-
-    if (StringUtils.isNotEmpty(apiKey)) {
-
-      System.out.println("bing chilling");
-      subsciption2 =  exchange
-              .getStreamingTradeService()
-              .getUserTrades(CurrencyPair.BTC_USD)
-              .subscribe(this::updateTrade);
-
-
-      subsciption3 =  exchange
-              .getStreamingTradeService()
-              .getOrderChanges(CurrencyPair.BTC_USD)
-              .subscribe(this::findAveragePrice);
-    }
-
-    try {
-      Thread.sleep(10000);
-    } catch (InterruptedException e) {
-      e.printStackTrace();
-    }
+    this.exchange.createSubscriptions(this);
+    state = State.FIND_INDICATOR;
   }
 
-  public synchronized void update(OrderBook book) {
+  public synchronized void shouldTrade(OrderBook book) {
     if(state == State.BUY) {
-      System.out.println("BUYING:");
-      OrderBookPriceCalculator buyPrice = new OrderBookPriceCalculator(book);
-
-      state = State.SELL;
-      String ID = "BID" + System.currentTimeMillis();
-      BigDecimal amount = new BigDecimal(".0001");
-      BigDecimal price = buyPrice.getAskPrice(amount)
-              .add(new BigDecimal(1))
-              .setScale(2, RoundingMode.HALF_UP);
-      LimitOrder buyOrder = new LimitOrder(
-              Order.OrderType.BID,amount, CurrencyPair.BTC_USD, ID, new Date(), price);
-
       try{
-        exchange.getTradeService().placeLimitOrder(buyOrder);
-      }catch(Exception ex) {
-        ex.printStackTrace();
-        state = State.WAITFORFILL;
+        this.exchange.attemptBuy(this.tradePrice, book);
+        state = State.WAIT_FOR_FULFILLED_BID;
+      }catch(IOException ex) {
+        System.err.println("ATTEMPTED BUY FAILED");
+        state = State.LEAVE;
       }
-      state = State.WAITFORFILL;
-    }
-    if(state == State.SELL) {
-      System.out.println("SELLING:");
-      OrderBookPriceCalculator sellPrice = new OrderBookPriceCalculator(book);
-
-      state = State.SELL;
-      String ID = "ASK" + System.currentTimeMillis();
-      BigDecimal amount = new BigDecimal(".0001");
-      BigDecimal price = sellPrice.getBidPrice(amount)
-              .add(new BigDecimal(1))
-              .setScale(2, RoundingMode.HALF_UP);
-      LimitOrder sellOrder = new LimitOrder(
-              Order.OrderType.ASK,amount, CurrencyPair.BTC_USD, ID, new Date(), price);
-
-      try{
-        exchange.getTradeService().placeLimitOrder(sellOrder);
-      }catch(Exception ex) {
-        ex.printStackTrace();
-        state = State.WAITFORSELLEND;
+    } else if(state == State.SELL) {
+      try {
+        this.exchange.attemptSell(this.tradePrice, book);
+        state = State.WAIT_FOR_FULFILLED_ASK;
+      }catch(IOException exception) {
+        System.err.println("ATTEMPTED SELL FAILED");
+        state = State.LEAVE;
       }
-      state = State.WAITFORSELLEND;
     }
   }
 
-  public void updateTrade(UserTrade trade) {
-    System.out.println(trade.toString());
-  }
 
-  public void findAveragePrice(Order order) {
-    if(order.getStatus().equals(Order.OrderStatus.FILLED) && state == State.SELL) {
+  public void orderUpdates(Order order) {
+    if(state == State.WAIT_FOR_FULFILLED_BID && order.getStatus().equals(Order.OrderStatus.FILLED)) {
       state = State.LEAVE;
-      System.out.println("POGGERS");
     }
-    System.out.println(order.toString());
-    if(order.getStatus().equals(Order.OrderStatus.FILLED) && state == State.WAITFORFILL) {
+
+    if(state == State.WAIT_FOR_FULFILLED_ASK && order.getStatus().equals(Order.OrderStatus.FILLED)) {
       this.state = State.SELL;
-      System.out.println("SELL TIME");
     }
+  }
+
+  public void tradeUpdates(Trade trade) {
+    System.out.println("COMPLETED TRADE: " + trade);
   }
 
   @Override
-  public void close() throws IOException {
-    exchange.disconnect().blockingAwait();
-    subscription.dispose();
-    subsciption2.dispose();
-    subsciption3.dispose();
+  public void close() {
+    this.exchange.closeSubscriptions();
   }
 
   public static void main(String[] args) {
-    Bot bot = new Bot();
+    Bot bot = new Bot(5);
     bot.init();
   }
 }
--- src/main/java/malakov/tradingbot/twitter/TwitterExplorer.java
+++ src/main/java/malakov/tradingbot/twitter/TwitterExplorer.java
@@ -26,14 +26,28 @@ import java.util.HashSet;
 import java.time.OffsetDateTime;
 
 public class TwitterExplorer {
-  private static final String elonNeg = "(elon musk) (moron OR bootlicker OR fraud OR little man OR epstein OR maxwell OR " +
-          "stupid OR bad OR idiot OR pathetic OR alt right OR nazi) (-genuis -tony -stark -thank -mr" +
-          " -mr. -hero -doge -smart -brilliant -good -richest";
-  private static final String elonPos = "(elon must) (genuis OR tony stark OR thank OR mr musk OR mr. musk OR hero OR doge OR " +
-          "smart OR brilliant OR good OR richest) (-moron -bootlicker -fraud -little -epstein -maxwell" +
-          "-stupid -bad -idiot -pathetic -alt -nazi)";
+  private final String elonNeg;
+          //= "(elon musk) (moron OR bootlicker OR fraud OR little man OR epstein OR maxwell OR " +
+          //"stupid OR bad OR idiot OR pathetic OR alt right OR nazi) (-genuis -tony -stark -thank -mr" +
+          //" -mr. -hero -doge -smart -brilliant -good -richest)";
+  private final String elonPos;
+          //= "(elon must) (genuis OR tony stark OR thank OR mr musk OR mr. musk OR hero OR doge OR " +
+          //"smart OR brilliant OR good OR richest) (-moron -bootlicker -fraud -little -epstein -maxwell" +
+          //"-stupid -bad -idiot -pathetic -alt -nazi)";
 
-  public static void main(String[] args) {
+  private TwitterApi apiInstance;
+
+  private final TwitterCredentialsBearer credentials;
+
+  public TwitterExplorer(String neg, String pos) {
+    elonNeg = neg;
+    elonPos = pos;
+    this.credentials = new TwitterCredentialsBearer(System.getenv("TWITTER_BEARER_TOKEN"));
+    apiInstance = new TwitterApi(credentials);
+  }
+
+
+  public void temp() {
     // Set the credentials based on the API's "security" tag values.
     // Check the API definition in https://api.twitter.com/2/openapi.json
     // When multiple options exist, the SDK supports only "OAuth2UserToken" or "BearerToken"
@@ -41,32 +55,34 @@ public class TwitterExplorer {
     // Uncomment and set the credentials configuration
 
     // Configure HTTP bearer authorization:
-    TwitterCredentialsBearer credentials = new TwitterCredentialsBearer(System.getenv("TWITTER_BEARER_TOKEN"));
-    TwitterApi apiInstance = new TwitterApi(credentials);
 
-    // Set the params values
-    Integer backfillMinutes = 56; // Integer | The number of minutes of backfill requested.
-    OffsetDateTime startTime = OffsetDateTime.parse("2021-02-01T18:40:40.000Z"); // OffsetDateTime | YYYY-MM-DDTHH:mm:ssZ. The earliest UTC timestamp from which the Tweets will be provided.
-    OffsetDateTime endTime = OffsetDateTime.parse("2021-02-14T18:40:40.000Z"); // OffsetDateTime | YYYY-MM-DDTHH:mm:ssZ. The latest UTC timestamp to which the Tweets will be provided.
-    Set<String> tweetFields = new HashSet<>(Arrays.asList()); // Set<String> | A comma separated list of Tweet fields to display.
-    Set<String> expansions = new HashSet<>(Arrays.asList()); // Set<String> | A comma separated list of fields to expand.
-    Set<String> mediaFields = new HashSet<>(Arrays.asList()); // Set<String> | A comma separated list of Media fields to display.
-    Set<String> pollFields = new HashSet<>(Arrays.asList()); // Set<String> | A comma separated list of Poll fields to display.
-    Set<String> userFields = new HashSet<>(Arrays.asList()); // Set<String> | A comma separated list of User fields to display.
-    Set<String> placeFields = new HashSet<>(Arrays.asList()); // Set<String> | A comma separated list of Place fields to display.
+
+
+
+    AddOrDeleteRulesRequest addOrDeleteRulesRequest = new AddOrDeleteRulesRequest();
+
+//    List<String> ids = Arrays.asList("1607460998146936833","1607460998146936832"); // List<String> | IDs of all deleted user-specified stream filtering rules.
+//    DeleteRulesRequest deleteRulesRequest = new DeleteRulesRequest();
+//    DeleteRulesRequestDelete deleteRules = new DeleteRulesRequestDelete();
+//
+//    deleteRules.ids(ids);
+//    deleteRulesRequest.delete(deleteRules);
+
+//    addOrDeleteRulesRequest.setActualInstance(deleteRulesRequest);
+
 
     List<RuleNoId> ruleList = new ArrayList<>();
     ruleList.add(new RuleNoId().value(elonNeg).tag("elon neg"));
     ruleList.add(new RuleNoId().value(elonPos).tag("elon pos"));
+
     AddRulesRequest addRulesRequest = new AddRulesRequest();
     addRulesRequest.add(ruleList);
-    DeleteRulesRequest delete = new DeleteRulesRequest();
-    AddOrDeleteRulesRequest delReq = new AddOrDeleteRulesRequest(delete); // AddOrDeleteRulesRequest |
-    AddOrDeleteRulesRequest addOrDeleteRulesRequest = new AddOrDeleteRulesRequest(addRulesRequest);
-//    addOrDeleteRulesRequest.getDeleteRulesRequest(delete);
+
+    addOrDeleteRulesRequest.setActualInstance(addRulesRequest);
+
     Boolean dryRun = false; // Boolean | Dry Run can be used with both the add and delete action, with the expected result given, but without actually taking any action in the system (meaning the end state will always be as it was when the request was submitted). This is particularly useful to validate rule changes.
     try {
-      AddOrDeleteRulesResponse result = apiInstance.tweets().addOrDeleteRules(delReq)
+      AddOrDeleteRulesResponse result = apiInstance.tweets().addOrDeleteRules(addOrDeleteRulesRequest)
               .dryRun(dryRun)
               .execute();
       System.out.println(result);
